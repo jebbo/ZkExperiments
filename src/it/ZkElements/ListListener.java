67b652634dc0a3dbb63772ea07482c2e3152e486
jebbo <jebbo85@gmail.com>
2011-01-12 21:38:15 +0100

    Signed-off-by: jebbo <jebbo85@gmail.com>

---
+++ src/it/ZkElements/ListListener.java
@@ -0,0 +1,28 @@
+package it.ZkElements;
+
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Listitem;
+
+class ListListener implements EventListener
+{
+	private Listbox l;
+	private Listbox r;
+	private Listitem temp;
+	
+	public ListListener (Listbox l, 
+			Listbox r, Listitem temp)
+	{
+		this.l = l;
+		this.r = r;
+		this.temp = temp;
+	}
+	
+	@Override
+	public void onEvent(Event event) throws Exception 
+	{
+		r.getItems().add(temp);
+		l.getItems().remove(temp);
+	}
+}
--- src/it/ZkElements/insertPerson.java
+++ src/it/ZkElements/insertPerson.java
@@ -5,12 +5,17 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zul.Combobox;
 import org.zkoss.zul.Datebox;
 import org.zkoss.zul.Decimalbox;
+import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Listitem;
 import org.zkoss.zul.Textbox;
 
 public class insertPerson extends GenericForwardComposer 
@@ -27,10 +32,14 @@ public class insertPerson extends GenericForwardComposer
 	Datebox birthday;
 	Textbox email;
 	Textbox note;
+	Listbox r;
+	Listbox l;
 	
 	@Override
 	public void doAfterCompose(Component comp) throws Exception 
 	{
+		HashMap catItem = new HashMap();
+		
 		super.doAfterCompose(comp);
 		
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
@@ -42,10 +51,21 @@ public class insertPerson extends GenericForwardComposer
 		Statement y = conn.createStatement();
 		ResultSet rs  = y.executeQuery("SELECT * FROM category ORDER BY name");
 		
-		/*while(rs.next())
+		while(rs.next())
 		{
-			category.
-		}*/
+			l.getItems().add(new Listitem(rs.getString(2)));	
+		}
+		
+		List ll = l.getItems();
+		
+		Iterator i = ll.iterator();
+		
+		while(i.hasNext())
+		{
+			Listitem temp = (Listitem)i.next();
+			
+			temp.addEventListener("onClick", new ListListener(l,r,temp)); 
+		}
 		
 		conn.close();
 	}
@@ -56,7 +76,6 @@ public class insertPerson extends GenericForwardComposer
 		Connection conn = DriverManager
 				.getConnection("jdbc:mysql://localhost/test?" +
                                    "user=root&password=");
-		
 		
 		Statement y = conn.createStatement();
 		
